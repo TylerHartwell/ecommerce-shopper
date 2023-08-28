5c0d7cfd7ca5513af2e5f90c0ea84619680268fa
TylerHartwell <thartwell37@gmail.com>
2023-08-28 16:29:12 -0700

    make quantity selector functional

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -57,6 +57,8 @@ document.querySelector(`.js-products-grid`).innerHTML = productsHTML
 document.querySelectorAll(`.js-add-to-cart`).forEach(button => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+        const addQuantity = Number(quantitySelector.value)
         let matchingItem
         cart.forEach(item => {
             if(productId === item.productId){
@@ -64,10 +66,10 @@ document.querySelectorAll(`.js-add-to-cart`).forEach(button => {
             }
         })
         if(matchingItem){
-            matchingItem.quantity++
+            matchingItem.quantity += addQuantity
         } else {
             cart.push({productId: productId,
-                quantity: 1
+                quantity: addQuantity
             })
         }
         let cartQuantity = 0
